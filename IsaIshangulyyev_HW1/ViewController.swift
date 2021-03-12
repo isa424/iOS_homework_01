3ffb9c9d776148cd25c188008169a6504576a6bf
Isa Ishangulyyev <isaishan424@gmail.com>
2021-03-13 00:22:48 +0300

    Initial Commit

---
+++ IsaIshangulyyev_HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IsaIshangulyyev_HW1
+//
+//  Created by CTIS Student on 13.03.2021.
+//  Copyright © 2021 CTIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
